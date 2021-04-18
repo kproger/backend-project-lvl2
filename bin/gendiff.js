b28ae08af6443d862a1479f87063b62627dd22ee
Bekaev Sharif <bekaevsharif@mail.ru>
2021-04-18 15:02:27 +0300

    cross changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-
-const { Command } = require('commander');
-const program = new Command();
-program.version('0.0.1');
+import program from 'commander';
+import genDiff from '../index.js';
 
 program
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format');
+    .option('-f, --format [type]', 'output format')
+    .version('0.0.1')
+    .action((filePath1, filePath2) => {
+        console.log(genDiff(filePath1, filePath2))
+    })
 
 program.parse(process.argv)
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import _ from "lodash";
+import fs from 'fs';
+import path from 'path';
+
+const makeAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
+const getFileData = (filePath) => fs.readFileSync(makeAbsolutePath(filePath), 'utf-8');
+
+const compareFlatFiles = (path1, path2) => {
+    const file1 = getFileData(path1);
+    const file2 = getFileData(path2);
+    
+    const allKeys = Object.keys(file1).concat(Object.keys(file2));
+    const uniqKeys = _.uniq(allKeys);
+    
+    return JSON.stringify(uniqKeys.reduce((acc, elem) => {
+        if (!_.has(file2, elem)) {
+            const oldKey = '- ' + elem;
+            acc[oldKey] = file1[elem];
+            return acc
+        } else if (file2[elem] !== file1[elem]) {
+            const oldKey = '- ' + elem;
+            const newKey = '+ ' + elem;
+            acc[oldKey] = file1[elem];
+            acc[newKey] = file2[elem]
+            return acc
+        } else {
+            return {...acc, [elem]: file2[elem]}
+        }
+    }, {}), null, ' ')
+}
+
+export default compareFlatFiles;
