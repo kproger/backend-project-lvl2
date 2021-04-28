5ee2738ede4d5e245693b004ea4fdef9d417a0ac
Muhammad Keraev <bekaevsharif@mail.ru>
2021-04-28 16:43:57 +0300

    incorrect diff

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ const makeAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 const getFileData = (filePath) => fs.readFileSync(makeAbsolutePath(filePath), 'utf-8');
 
 const compareFlatFiles = (path1, path2) => {
-    const file1 = getFileData(path1);
-    const file2 = getFileData(path2);
-    
+    // const file1 = getFileData(path1);
+    // const file2 = getFileData(path2);
+
+    const file1 = fs.readFileSync(path.resolve(path1), 'utf-8');
+    const file2 = fs.readFileSync(path.resolve(path2), 'utf-8');
+
     const allKeys = Object.keys(file1).concat(Object.keys(file2));
-    const uniqKeys = _.uniq(allKeys);
-    
-    return JSON.stringify(uniqKeys.reduce((acc, elem) => {
+    const sortedUniqKeys = _.uniq(allKeys).sort();
+
+    return JSON.stringify(sortedUniqKeys.reduce((acc, elem) => {
         if (!_.has(file2, elem)) {
             const oldKey = '- ' + elem;
             acc[oldKey] = file1[elem];
@@ -24,9 +27,14 @@ const compareFlatFiles = (path1, path2) => {
             acc[newKey] = file2[elem]
             return acc
         } else {
-            return {...acc, [elem]: file2[elem]}
+            return {
+                ...acc,
+                ['  ' + elem]: file2[elem]
+            }
         }
     }, {}), null, ' ')
-}
+};
+
+
 
 export default compareFlatFiles;
