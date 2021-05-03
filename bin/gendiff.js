52252711e046ec7dbd2dc23526f310d0708892e8
Muhammad Keraev <bekaevsharif@mail.ru>
2021-05-03 23:52:42 +0300

    pretesting stage

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,12 +3,12 @@ import program from 'commander';
 import genDiff from '../index.js';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format')
-    .version('0.0.1')
-    .action((filePath1, filePath2) => {
-        console.log(genDiff(filePath1, filePath2))
-    })
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .version('0.0.1')
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2));
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
@@ -6,30 +6,29 @@ const makeAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 const getFileData = (filePath) => fs.readFileSync(makeAbsolutePath(filePath), 'utf-8');
 
 const compareFlatFiles = (path1, path2) => {
-    const file1 = JSON.parse(getFileData(path1));
-    const file2 = JSON.parse(getFileData(path2));
+  const file1 = JSON.parse(getFileData(path1));
+  const file2 = JSON.parse(getFileData(path2));
 
-    const allKeys = Object.keys(file1).concat(Object.keys(file2));
-    const sortedUniqKeys = _.uniq(allKeys).sort();
+  const allKeys = Object.keys(file1).concat(Object.keys(file2));
+  const sortedUniqKeys = _.uniq(allKeys).sort();
 
-    return JSON.stringify(sortedUniqKeys.reduce((acc, elem) => {
-        if (!_.has(file2, elem)) {
-            const oldKey = '- ' + elem;
-            acc[oldKey] = file1[elem];
-            return acc
-        } else if (file2[elem] !== file1[elem]) {
-            const oldKey = '- ' + elem;
-            const newKey = '+ ' + elem;
-            acc[oldKey] = file1[elem];
-            acc[newKey] = file2[elem]
-            return acc
-        } else {
-            return {
-                ...acc,
-                ['  ' + elem]: file2[elem]
-            }
-        }
-    }, {}), null, ' ')
+  return JSON.stringify(sortedUniqKeys.reduce((acc, elem) => {
+    if (!_.has(file2, elem)) {
+      const oldKey = `- ${elem}`;
+      acc[oldKey] = file1[elem];
+      return acc;
+    } if (file2[elem] !== file1[elem]) {
+      const oldKey = `- ${elem}`;
+      const newKey = `+ ${elem}`;
+      acc[oldKey] = file1[elem];
+      acc[newKey] = file2[elem];
+      return acc;
+    }
+    return {
+      ...acc,
+      [`  ${elem}`]: file2[elem],
+    };
+  }, {}), null, ' ');
 };
 
 export default compareFlatFiles;
