0f160be6017f9bb2f46fc16d0434447c18228af0
Muhammad Keraev <bekaevsharif@mail.ru>
2021-05-04 00:07:28 +0300

    ss

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,24 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { dirname } from 'path';
+import { test, expect } from 'jest';
 import gendiff from '../index.js';
-import {test, expect} from 'jest';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const json1 = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
-  };
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
 
-  const json2 = {
-    "timeout": 20,
-    "verbose": true,
-    "host": "hexlet.io"
-  };
+const json2 = {
+  timeout: 20,
+  verbose: true,
+  host:  'hexlet.io',
+};
 
-  test('base check', () => {
-      expect(gendiff(json1, json2)).toEqual(JSON.stringify())
-  })
+test('base check', () => {
+  expect(gendiff(json1, json2)).toEqual(JSON.stringify());
+});
